edab203fa6751d423fbb8be543d1423bf01bc4f9
WellingtonMarques96 <well1506marques@gmail.com>
2023-01-12 23:12:27 -0300

    jS 2/3

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const links = document.querySelectorAll(".item");
 const submit = document.querySelector(".botao");
 const selected = document.querySelector("#selected");
 const button = document.querySelector("button");
-const message = document.querySelector("#message");
+const message = document.querySelector("message");
+const links = document.querySelectorAll(".item");
 
 function mostrar() {
   const thanks = document.querySelector(".thank-you");
@@ -12,3 +12,14 @@ function mostrar() {
 }
 
 submit.addEventListener("click", mostrar);
+
+function changeColor(event) {
+  links.forEach((link) => {
+    link.classList.remove("selected");
+  });
+  event.currentTarget.classList.add("selected");
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", changeColor);
+});
